d626dd219557bbf9e4d840eca93fd21553e29343
Andrea Pineda <andreapineda117@outlook.com>
2023-07-14 20:33:52 -0600

    community events carousel

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const progressBar = document.getElementById("progress-bar");
 
-window.addEventListener("scroll", () =>{
+window.addEventListener("scroll", () => {
     let height = document.body.scrollHeight - window.innerHeight;
     let scrollPosition = document.documentElement.scrollTop;
-    let width = (scrollPosition/height) * 100;
-    progressBar.style.width =`${width}%`;
+    let width = (scrollPosition / height) * 100;
+    progressBar.style.width = `${width}%`;
 })
 
-console.log("connected");
+
+// Valentine's Mode
+var checkbox = document.getElementById('heart');
+checkbox.addEventListener("change", validaCheckbox, false);
+function validaCheckbox() {
+    var checked = checkbox.checked;
+    if (checked) {
+        setTheme('dark')
+    } else {
+        setTheme('light')
+    }
+}
+
+window.addEventListener('load', function () {
+    setTheme('light')
+});
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme)
+}
+
+const toggleBtn = document.getElementById('heart')
+toggleBtn.addEventListener('click', () => {
+    let switchTheme = localStorage.getItem('theme') == 'dark' ? 'light' : 'dark'
+    setTheme(switchTheme)
+    document.documentElement.classList.toggle('dark')
+})
